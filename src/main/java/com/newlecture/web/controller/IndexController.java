4388374ca29f811bf7d9875aa27c81f9154f69f4
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-05-05 23:44:24 +0900

    ViewResolver 사용하기

--- src/main/java/com/newlecture/web/controller/IndexController.java
+++ src/main/java/com/newlecture/web/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.newlecture.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -11,9 +10,9 @@ public class IndexController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("data", "Hello Spring MVC!");
-		mv.setViewName("/WEB-INF/view/index.jsp");
+		//mv.setViewName("/WEB-INF/view/index");
 		return mv;
 	}
 	
