cc02eba5ce57125d56d157c2e17212ed21c71b2b
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-05-11 01:23:21 +0900

    POST 입력방법

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -15,9 +15,9 @@ public class NoticeController {
 	
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg() {
+	public String reg(String title, String content) {
 		
-		return "a";
+		return String.format("title:%s<br>content:%s<br>", title, content);
 	}
 	
 	@RequestMapping("edit")
