46816abda14fcf1c5731550e26ca0f7561fdcce2
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-05-10 20:40:22 +0900

    Annotation으로 서비스 객체 생성하기

--- src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
+++ src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
@@ -12,11 +12,14 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.entity.NoticeView;
 import com.newlecture.web.service.NoticeService;
 
+@Service // @Controller, @Service, @Repository
 public class JDBCNoticeService implements NoticeService {
 	
 	@Autowired
