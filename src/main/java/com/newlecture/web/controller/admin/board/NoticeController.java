250091130702ad6efed381e1f1be5377c230e5a6
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-05-11 14:54:16 +0900

    POST 입력 #6(물리경로 얻기와 파일 저장하기)

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,5 +1,12 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
 	
+	@Autowired
+	private ServletContext ctx;
+	
 	@RequestMapping("list")
 	public String list() {
 		return "";
@@ -16,11 +26,25 @@ public class NoticeController {
 	
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food) {
+	public String reg(String title, String content, MultipartFile file, String category, String[] foods, String food, HttpServletRequest request) throws IllegalStateException, IOException {
 		
 		long size = file.getSize();
 		String fileName = file.getOriginalFilename();
 		System.out.printf("fileName:%s, fileSize:%d\n", fileName, size);
+		
+		//ServletContext ctx = request.getServletContext();
+		String webPath = "/static/upload";
+		String realPath = ctx.getRealPath(webPath);
+		System.out.printf("realPath : %s\n", realPath);
+		// 업로드 하기 위한 경로가 없을 경우
+		File savePath = new File(realPath);
+		if(!savePath.exists())
+			savePath.mkdirs();
+		
+		realPath += File.separator + fileName;
+		File savaFile = new File(realPath);
+		
+		file.transferTo(savaFile);
 		
 		for(String f : foods)
 			System.out.println(f);
